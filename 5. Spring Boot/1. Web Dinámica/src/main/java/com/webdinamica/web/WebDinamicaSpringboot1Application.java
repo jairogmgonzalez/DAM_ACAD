37d2feb1ba051ceef774b9dc6d270a241ebf55fb
jairogmgonzalez <jairogmgonzalez@gmail.com>
2024-12-01 20:10:47 +0100

    Tema 5 Ej 1

---
+++ 5. Spring Boot/1. Web Dinámica/src/main/java/com/webdinamica/web/WebDinamicaSpringboot1Application.java
@@ -0,0 +1,13 @@
+package com.webdinamica.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDinamicaSpringboot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebDinamicaSpringboot1Application.class, args);
+	}
+
+}
---
+++ 5. Spring Boot/1. Web Dinámica/src/test/java/com/webdinamica/web/WebDinamicaSpringboot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webdinamica.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDinamicaSpringboot1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
